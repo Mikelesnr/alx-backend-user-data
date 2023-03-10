869e06943f8c92a1ff6da59362d43e487e1afddb
Mikelesnr <mwanza.n.m@gmail.com>
2023-03-10 04:02:13 +0200

    basic auth corrections

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
-"""
-API authentication module
-"""
-
+""" Auth class, Require auth with stars """
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
-    """ Authentication """
-
+class Auth():
+    """ manage the API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Checks if API routes require authentication """
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """ require authorithation """
+        if path is None or excluded_paths is None or not len(excluded_paths):
             return True
         if path[-1] != '/':
             path += '/'
+        for i in excluded_paths:
+            if i.endswith('*'):
+                if path.startswith(i[:1]):
+                    return False
         if path in excluded_paths:
             return False
         else:
             return True
 
     def authorization_header(self, request=None) -> str:
-        """ Checks if Authorization request header is present
-        & contains values """
-        if request is None or "Authorization" not in request.headers:
+        """ authorization header """
+        if request is None:
             return None
-        else:
-            return request.headers.get('Authorization')
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ placeholder """
+        """ current user """
         return None
